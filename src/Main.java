06a489a2557be3c13e17930eceec56ad61bd61da
Prosenjit Mondal <contact.prosen25@gmail.com>
2024-12-11 16:20:32 +0000

    implemented logic to calculate number paint bucket required

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
+
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(7.25, 4.3, 2.35));
+
+        System.out.println(getBucketCount(3.4, 1.5));
+        System.out.println(getBucketCount(6.26, 2.2));
+        System.out.println(getBucketCount(3.26, 0.75));
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBucket) {
+
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBucket < 0) {
+            return -1;
+        }
+
+        double areaToPaint = width * height;
+        int numberOfBucketNeeded = (int) Math.ceil(areaToPaint / areaPerBucket);
+
+        return numberOfBucketNeeded - extraBucket;
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+
+        double areaToPaint = width * height;
+
+        return (int) Math.ceil(areaToPaint / areaPerBucket);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+
+        return (int) Math.ceil(area / areaPerBucket);
+    }
+}
